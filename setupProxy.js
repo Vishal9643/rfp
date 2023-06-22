add57d516ccabe0b22ac27f01fa452abd4c7d89a
vishal9643 <amitkrmishra864@gmail.com>
2023-06-22 14:01:58 +0530

    changes

--- setupProxy.js
+++ setupProxy.js
@@ -1,7 +1,5 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
-//function to setup proxy to call the backend server
-
 module.exports = function (app) {
   app.use(
     "/Auth",
