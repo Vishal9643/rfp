8e9affa824dad969d51e44ca7c22bc597f2002aa
vishal9643 <amitkrmishra864@gmail.com>
2023-06-23 11:40:37 +0530

    changes

--- src/pages/common_component/Login.jsx
+++ src/pages/common_component/Login.jsx
@@ -233,7 +233,7 @@ const Login = () => {
                             </Link>
                           </div>
                           <div className="mt-4 text-center">
-                            <Link to="/forget-password" className="text-muted">
+                            <Link to="/reset-password" className="text-muted">
                               <i className="mdi mdi-lock mr-1"></i> Forgot your
                               password?
                             </Link>
